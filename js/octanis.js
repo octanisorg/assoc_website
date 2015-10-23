91cb1ab26071b43c0cdabd6a16f0d5e864c1fe01
Sam Sulaimanov <sam@bitmorse.com>
2015-10-23 21:13:04 +0200

    added eps pic and callouts

--- js/octanis.js
+++ js/octanis.js
@@ -1,3 +1,25 @@
+var isMobile = {
+    Android: function() {
+        return navigator.userAgent.match(/Android/i);
+    },
+    BlackBerry: function() {
+        return navigator.userAgent.match(/BlackBerry/i);
+    },
+    iOS: function() {
+        return navigator.userAgent.match(/iPhone|iPad|iPod/i);
+    },
+    Opera: function() {
+        return navigator.userAgent.match(/Opera Mini/i);
+    },
+    Windows: function() {
+        return navigator.userAgent.match(/IEMobile/i);
+    },
+    any: function() {
+        return (isMobile.Android() || isMobile.BlackBerry() || isMobile.iOS() || isMobile.Opera() || isMobile.Windows());
+    }
+};
+
+
 $(function() {
     var pull        = $('#navpull');
         menu        = $('ul.main-nav');
@@ -38,5 +60,40 @@ $(function() {
         }
       });
 
+    if( !isMobile.any() ){
+        $.stellar({
+            // Set scrolling to be in either one or both directions
+            horizontalScrolling: false,
+            verticalScrolling: true,
 
+            // Set the global alignment offsets
+            horizontalOffset: 0,
+            verticalOffset: 0,
+
+            // Refreshes parallax content on window load and resize
+            responsive: true,
+
+            // Select which property is used to calculate scroll.
+            // Choose 'scroll', 'position', 'margin' or 'transform',
+            // or write your own 'scrollProperty' plugin.
+            scrollProperty: 'scroll',
+
+            // Select which property is used to position elements.
+            // Choose between 'position' or 'transform',
+            // or write your own 'positionProperty' plugin.
+            positionProperty: 'position',
+
+            // Enable or disable the two types of parallax
+            parallaxBackgrounds: false,
+            parallaxElements: true,
+
+            // Hide parallax elements that move outside the viewport
+            hideDistantElements: false,
+
+            // Customise how elements are shown and hidden
+            hideElement: function($elem) { $elem.hide(); },
+            showElement: function($elem) { $elem.show(); }
+        });
+    }
+      
 });
