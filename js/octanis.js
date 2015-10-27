3ec54ea1a31165ace55b4ec769f172066f9fa241
Sam Sulaimanov <sam@bitmorse.com>
2015-10-27 01:34:04 +0100

    Fixing paths

--- js/octanis.js
+++ js/octanis.js
@@ -71,7 +71,7 @@ $(function() {
             verticalOffset: 0,
 
             // Refreshes parallax content on window load and resize
-            responsive: true,
+            responsive: false,
 
             // Select which property is used to calculate scroll.
             // Choose 'scroll', 'position', 'margin' or 'transform',
