617c6b48a2690436ea917433ec8589840bc94044
Sam Sulaimanov <sam@bitmorse.com>
2017-02-20 19:16:54 +0100

    racks

--- _rackapp/src/components/Splash.vue
+++ _rackapp/src/components/Splash.vue
@@ -1,7 +1,27 @@
 <template>
-  <div id="hello">
-     <router-link to="items"class="btn btn--large btn--default cta">Get Started</router-link>
+
+<div class="dimmed">
+
+  <header class="clearfix full padded">
+      <div class="logo pull-left">
+        <h1 class="mrg0"><a href="/"><img src="http://octanis.org/img/white_logo.png"></a>   </h1>
+      </div>
+
+      <div class="masterhead relative-parent">
+        <h1 class="oc-brand-indicator">Octanis Rack</h1><br/><br/>
+        <router-link to="items"class="btn btn--large btn--default cta">Get Started</router-link>
+      </div>
+
+  </header>
+
+
+  <div id="pagebg">
+    <img src="http://octanis.org/img/header1.jpg" alt="">
   </div>
+
+
+</div>
+
 </template>
 
 <script>
